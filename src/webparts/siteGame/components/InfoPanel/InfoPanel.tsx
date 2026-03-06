fc69044e08baa26af04789deae5c2bccc27b1bd2
Saurabh Tripathi <saurabh7019@gmail.com>
2026-03-07 00:01:36 +0100

    Add Luise. Closes #16 (#30)
    Co-authored-by: Adam Wójcik <58668583+Adam-it@users.noreply.github.com>

--- src/webparts/siteGame/components/InfoPanel/InfoPanel.tsx
+++ src/webparts/siteGame/components/InfoPanel/InfoPanel.tsx
@@ -370,6 +370,7 @@ export class InfoPanel extends React.Component<IInfoPanelProps, IPanelState> {
       pnp_spfx_samples: '📚',
       pnp_powershell: '🖥️',
       power_automate: '⚡',
+      luise: '👑',
     };
 
     const logoMap: Record<string, string> = {
